d743c1739e399f0d2ce0377af3a91740760df01a
1hyphend <Ivanc221@nycstudents.net>
2025-09-08 10:22:28 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,17 @@ import turtle
 from turtle import *
 t = Turtle()
 
+def message(input):
+    print(input)
+message("Hello Class")
 
 t.shape('turtle')
 
 t.forward(200)
-
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
 turtle.done()
