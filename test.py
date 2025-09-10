78fcba2cccf32a4c0a8dfa6798aec38f116c2af4
1hyphend <Ivanc221@nycstudents.net>
2025-09-10 07:30:41 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,24 @@
 import turtle
 from turtle import *
 t = Turtle()
+# #define the function name it then pass arguments/parameters
+# def add(x,y):
+#     print(x+y)
+# # add(15+5)
+# def rectangle(x):
+#     t.forward(125)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(90)
 
-def message(input):
-    print(input)
-message("Hello Class")
+#     square (50)
+#     turtle.done()
+#     for i in range(1,4)
+#     print(i)
 
-t.shape('turtle')
-
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
-t.left(90)
-t.forward(200)
+for i in range(0,2):
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
 turtle.done()
