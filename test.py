90fd16d59b94e87b305e1cf20e0990203c4b70e7
1hyphend <Ivanc221@nycstudents.net>
2026-02-01 13:19:08 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,17 +6,17 @@ t.speed(100)
 # #define the function name it then pass arguments/parameters
 # def add(x,y):
 #     print(x+y)
-# # add(15+5)
-# def rectangle(x):
-#     t.forward(125)
-#     t.left(90)
-#     t.forward(100)
-#     t.left(90)
+# add(15+5)
+def rectangle(x):
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
 
-#     square (50)
-#     turtle.done()
-#     for i in range(1,4)
-#     print(i)
+    rectangle(50)
+    turtle.done()
+    for i in range(1,4):
+        print(i)
 
 # for i in range(3):
 #     t.width(10)
